15b19522148b5610723e91b9727dd993d5911e18
Kai T. Ohlhus <k.ohlhus@gmail.com>
2018-10-07 23:13:28 +0200

    Some further work on the benchmarks.

---
+++ benchmarks/bm_run.m
@@ -0,0 +1,21 @@
+function bm_run ()
+
+%bm_setup ()
+
+[bm_list, options] = bm_collector ();
+
+done = 0;
+for i = 1:length(bm_list)
+  try
+    eval (bm_list(i).setup);
+  catch
+    fprintf ('FAIL %3d\n', i);
+    continue;
+  end
+  fprintf ('Done %3d\n', i);
+  done = done + 1;
+end
+
+fprintf ('\nDone %3d/%3d\n', done, length(bm_list));
+
+end
