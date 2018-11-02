4c4b1b615493149cbc2adc8be285a8caafa82b97
Kai T. Ohlhus <k.ohlhus@gmail.com>
2018-11-02 11:07:09 +0100

    @vsdp_benchmark.export_default_tables(): New function for standard reports.

---
+++ benchmarks/@vsdp_benchmark/export_default_tables.m
@@ -0,0 +1,8 @@
+function export_default_tables (obj, outdir)
+% EXPORT_DEFAULT_TABLES  Generates some standard tables for publication.
+%
+
+libs = unique ([obj.BENCHMARK.lib]);
+for i = 1:length (libs)
+end
+end
